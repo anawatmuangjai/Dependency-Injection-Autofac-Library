d2ec2eabb02003277ebb9b2bb8fe8c38166e745b
anawatmuangjai <anawat.muangjai@gmail.com>
2017-10-16 21:46:38 +0700

    Add Autofac Registering Type example

---
+++ DependencyInjection/RegisteringTypes/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace RegisteringTypes
+{
+    public interface ILog
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Engine
+    {
+        private ILog _log;
+        private int _id;
+
+        public Engine(ILog log)
+        {
+            _log = log;
+            _id = new Random().Next();
+        }
+
+        public void Ahead(int power)
+        {
+            _log.Write($"Engine [{_id}] ahead {power}");
+        }
+    }
+
+    public class Car
+    {
+        private Engine _engine;
+        private ILog _log;
+
+        public Car(Engine engine, ILog log)
+        {
+            _engine = engine;
+            _log = log;
+        }
+
+        public void Go()
+        {
+            _engine.Ahead(200);
+            _log.Write("Car going forward...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleLog>().As<ILog>().AsSelf();
+            builder.RegisterType<Engine>();
+            builder.RegisterType<Car>();
+
+            IContainer container = builder.Build();
+
+            var car = container.Resolve<Car>();
+            car.Go();
+        }
+    }
+}
